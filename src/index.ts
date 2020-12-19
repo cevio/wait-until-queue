98dfcc576272c158456b2f13a92253471e2945be
evio <evio@vip.qq.com>
2020-12-19 16:47:05 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,9 @@ export class WaitUntil {
   public wait<T = any>(callback: () => Promise<T> | T): Promise<T> {
     switch (this.status) {
       case 0:
-        this.status = 100;
-        return this.runTask<T>(callback);
+        this.pause();
+        this.runTask<T>(callback);
+        return this.pushTask();
       case 100: return this.pushTask<T>();
       case 200: return Promise.resolve<T>(this.result);
       case 500: return Promise.reject(this.error);
@@ -21,14 +22,10 @@ export class WaitUntil {
 
   private runTask<T = any>(callback: () => Promise<T> | T) {
     return Promise.resolve<T>(callback()).then((data) => {
-      this.status = 200;
-      this.result = data;
-      for (const { resolve } of this.stacks) resolve(data);
+      this.resolve(data);
       return data;
     }).catch(e => {
-      this.status = 500;
-      this.error = e;
-      for (const { reject } of this.stacks) reject(e);
+      this.reject(e);
       return Promise.reject(e);
     });
   }
@@ -39,7 +36,30 @@ export class WaitUntil {
     });
   }
 
+  public pause() {
+    this.status = 100;
+  }
+
+  public resolve(data: any) {
+    if (this.status === 100) {
+      this.status = 200;
+      this.result = data;
+      for (const { resolve } of this.stacks) resolve(data);
+      this.stacks.clear();
+    }
+  }
+
+  public reject(e: any) {
+    if (this.status === 100) {
+      this.status = 500;
+      this.error = e;
+      for (const { reject } of this.stacks) reject(e);
+      this.stacks.clear();
+    }
+  }
+
   public reset() {
+    if ([200, 500].indexOf(this.status) === -1) throw new Error('task not ready, you cannot reset');
     this.status = 0;
     this.error = null;
     this.result = null;
